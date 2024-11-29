d461f1d25621efd6f0040c5daba5d099842b51d3
yasindu2 <yasindu@petpos.com>
2024-11-29 07:58:57 +0530

    skill section

--- src/Components/AboutMe/Aboutme.jsx
+++ src/Components/AboutMe/Aboutme.jsx
@@ -3,6 +3,10 @@ import Header from "../Header/Header";
 import rusl from "../../Images/rusl.jpg";
 
 function Aboutme() {
+  const items = ['Js', 'HTML', 'CSS', 'React'];
+  const itemss = [{name:'yasindu',age:25},{name:'yasindu1',age:251},{name:'yasindu3',age:2512}];
+
+
   return (
     <div>
       <div className="container">
@@ -22,6 +26,23 @@ function Aboutme() {
             </div>
             <div className="image-container1">
               <img src={rusl} alt="image" className="image2"></img>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="row">
+            <div className="skills">
+        
+            {items.map((value,index)=>(
+                <div className="skill-item">
+                  {/* {items} */}
+                
+                <div className="icon">icon</div>
+                <div className="title-icon">{value}</div>
+                
+                </div>
+            ))}
             </div>
           </div>
         </section>
